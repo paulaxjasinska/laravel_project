3f18693707fbcc1b0220bfbaf2e9f325d7481a41
paulaxjasinska <paulinajasinska2003@wp.pl>
2023-06-04 19:16:30 +0200

    update layouts

--- my-app/resources/views/index.blade.php
+++ my-app/resources/views/index.blade.php
@@ -5,9 +5,36 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Plastic Surgery Clinic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .contentWrapper {
+          height: 100vh;
+          overflow: hidden;
+        }
+
+        .item1 {
+          background-image: url("1.jpg");
+          height: 95vh;
+          background-size: cover;
+          background-position: right 35% bottom 45%;
+        }
+
+        .item2 {
+          background-image: url("2.jpg");
+          height: 95vh;
+          background-size: cover;
+          background-position: right 35% bottom 45%;
+        }
+
+        .item3 {
+          background-image: url("3.jpg");
+          height: 95vh;
+          background-size: cover;
+          background-position: right 35% bottom 45%;
+        }
+      </style>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-light position-absolute w-100" style="z-index:10">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">Home</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,47 +61,45 @@
         </div>
       </div>
     </nav>
-    <div id="carouselExampleCaptions" class="carousel slide overflow-hidden"  data-bs-ride="false" >
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="1.jpg" class="d-block w-100" alt="..." style="height: 85vh;">
-          <div class="carousel-caption d-none d-md-block">
-            <h2 style="color: black">The best doctors</h2>
-            <p style="color: black">Doctors work with pleasure and with great experience.</p>
+    <div class="contentWrapper">
+        <div id="carouselExampleCaptions" class="carousel slide overflow-hidden"  data-bs-ride="false" >
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="item1 w-100">
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="item2 w-100">
+
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="item3 w-100">
+
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
           </div>
-        </div>
-        <div class="carousel-item">
-          <img src="2.jpg" class="d-block w-100" alt="..." style="height: 85vh;">
-          <div class="carousel-caption d-none d-md-block">
-            <h2 style="color: black">The best clinics</h2>
-            <p style="color: black">Available clinics nationwide with amazing service.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="3.jpg" class="d-block w-100" alt="..." style="height: 85vh;">
-          <div class="carousel-caption d-none d-md-block">
-            <h2 style="color: black">Well done treatments</h2>
-            <p style="color: black">You will surely be satisfied with our services that we offer you.</p>
-          </div>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+          <footer class="container-fluid bg-light">
+            <div class="row text-center pt-2">
+                <p class="text-dark">&copy; Plastic Surgery Clinic</p>
+            </div>
+        </footer>
     </div>
 
-
-    <footer class="container-fluid bg-light">     <div class="row text-center pt-2">         <p class="text-dark">&copy; Plastic Surgery Clinic</p>     </div> </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
       </body>
 </html>
