41b3c12092449bd946abec1f10ef2f11ac0cec8e
paulaxjasinska <paulinajasinska2003@wp.pl>
2023-06-07 11:51:13 +0200

    Move nav to separate component

--- my-app/resources/views/index.blade.php
+++ my-app/resources/views/index.blade.php
@@ -34,50 +34,7 @@
       </style>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-light position-absolute w-100" style="z-index:10">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/clinics">Clinics</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Treatments
-              </a>
-              <ul class="dropdown-menu">
-                @auth
-            <li style="text-align: center;">
-                    Hello {{auth()->user()->name}}
-            </li>
-            @if (auth()->user()->role === 'doctor')
-               <a href="/visits" style="color: black; text-align: center;"><li>Your visits</li></a>
-            @endif
-            <li>
-                <form class="inline" method="POST" action="/logout">
-                    @csrf
-                    <button type="submit" style="text-align: center;">
-                        Log out
-                    </button>
-                </form>
-            </li>
-            @else
-            <li><a class="dropdown-item" href="/appointments">Make an appointment</a></li>
-            <li><a class="dropdown-item" href="/login">Log in</a></li>
-            @endauth
-                <li><hr class="dropdown-divider"></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <x-nav/>
     <div class="contentWrapper">
         <div id="carouselExampleCaptions" class="carousel slide overflow-hidden"  data-bs-ride="false" >
             <div class="carousel-indicators">
--- my-app/resources/views/users/login.blade.php
+++ my-app/resources/views/users/login.blade.php
@@ -7,53 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body style="background-color: #99CCFF;">
-    <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/clinics">Clinics</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Treatments
-          </a>
-          <ul class="dropdown-menu">
-            @auth
-            <li style="text-align: center;">
-                    Hello {{auth()->user()->name}}
-            </li>
-            <li>
-                <form class="inline" method="POST" action="/logout">
-                    @csrf
-                    <button type="submit" style="text-align: center;">
-                        Log out
-                    </button>
-                </form>
-            </li>
-            @else
-            <li><a class="dropdown-item" href="/appointments">Make an appointment</a></li>
-            <li><a class="dropdown-item" href="/login">Log in</a></li>
-            @endauth
-
-            <li><hr class="dropdown-divider"></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</div>
-<br>
-<br>
-<br>
-<br>
+    <x-nav/>
 <div class="container">
     <div class="col-4 offset-4">
       <form class="d-flex flex-column justify-content-center" method="POST" action="/users/authenticate">
